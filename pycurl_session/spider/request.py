9cddc83003d57415594e53c5861ea03bdbfeefe4
Ace Lee <lzgug2@outlook.com>
2024-01-03 15:46:41 +0800

    update: FormRequest FormRequest.from_response() remove param file. use '@path' in formdata instead

--- pycurl_session/spider/request.py
+++ pycurl_session/spider/request.py
@@ -39,42 +39,19 @@ class Request(object):
 
 
 class FormRequest(Request):
-    def __init__(self, url, callback, meta={}, dont_filter=False, **args):
-        self.url = url
-        self.callback = callback
-        self.meta = meta
-        self.dont_filter = dont_filter
-        self.args = args
-        method_set = False
-        for k, v in args.items():
-            if k.lower == "method": method_set = True
-        if method_set == False:
-            self.args.update({"method": "POST"})
-        if "formdata" in args:
-            self.args.update({"data": args['formdata']})
-            self.args.pop("formdata")
+    def __init__(self, url, **args):
+        super().__init__(self, url, **args)
 
     @classmethod
-    def from_response(
-        self,
-        response: Response,
-        formid=None,
-        formname="",
-        formxpath="",
-        formcss="",
-        formnumber=0,
-        method="POST",
-        action=None,
-        formdata=None,
-        files=None,
-        callback=None,
+    def from_response(cls, response: Response,
+        formid=None, formname="", formxpath="", formcss="", formnumber=0,
+        method="POST", action=None, formdata=None, callback=None,
         **args
     ):
         if not isinstance(response, Response):
             raise Exception("Wrong Response")
         form = response._get_form(formid, formname, formxpath, formcss, formnumber)
         action = response._get_form_action(form, action)
-        _inputs = form.xpath(".//input").getall()
         _form_data = response._get_form_inputs(form)
         if isinstance(formdata, dict):
             _form_data.update(formdata)
@@ -85,11 +62,10 @@ class FormRequest(Request):
             else:
                 action = action + "?" + query
             _form_data = None
-        return Request(
-            url=action,
-            method=method,
-            data=_form_data,
-            files=files,
-            callback=callback,
-            **args
-        )
+        request_args = {
+            "method": method,
+            "data": _form_data,
+            "callback": callback,
+        }
+        request_args.update(args)
+        return Request(url=action, **request_args)
